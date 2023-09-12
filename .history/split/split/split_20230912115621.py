66a4067b52e33db0b585e9235d7265103249d597
giorgio <XRLAB@outlook.com>
2023-09-12 16:34:05 +0200

    whatever

---
+++ .history/split/split/split_20230912115621.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+import os
+from utils import *
+
+#filepath = '../excel_in/example_merged.xlsx'
+filepath = '../excel_in/AU5000-GDAT-ASSY.xls'
+output_path = '../excel_out'
+
+# split_excel(filepath, output_path)
+split_excel_xlwings(filepath, output_path)
+
+
+
+
---
+++ split/split/utils.py
@@ -0,0 +1,59 @@
+import pandas as pd 
+import PIL 
+from openpyxl import load_workbook, Workbook
+import xlwings as xw
+import os 
+
+def split_excel(multisheet_excel_file, output_path):
+    """
+        Description: A simple function to wrap pandas methods and split all the sheets present in an 
+                     excel file into multiple single sheet files
+
+    	Args:
+            - multisheet_excel_filepath: an excel file containing multiple sheets (tabs)
+            - output_path: path to output the split excel files
+            
+        Outputs: 
+            - originalfilename_sheetname.xlsx
+    """
+
+    file_name = os.path.basename(multisheet_excel_file)
+    base_name, _ = os.path.splitext(file_name)
+
+    excel_in = pd.ExcelFile(multisheet_excel_file)
+    sheets = excel_in.sheet_names
+
+    for sheet in sheets:
+        df = pd.read_excel(excel_in, sheet_name=sheet)  
+        df.to_excel(f'{output_path}/{base_name}_{sheet}.xlsx', index=False)
+
+
+def split_excel_xlwings(multisheet_excel_file, output_path):
+    """
+        Description: A simple function to split all the sheets present in an 
+                     excel file into multiple single sheet files, using the xlwings backend 
+                     for more complex file (images, color data, etc)
+
+    	Args:
+            - multisheet_excel_filepath: an excel file containing multiple sheets (tabs)
+            - output_path: path to output the split excel files
+            
+        Outputs: 
+            - originalfilename_sheetname.xlsx
+    """
+
+    file_name = os.path.basename(multisheet_excel_file)
+    base_name, _ = os.path.splitext(file_name)
+
+    try: 
+        excel_app = xw.App(visible=False)
+        wb = excel_app.books.open(multisheet_excel_file)
+
+        for sheet in wb.sheets:
+            wb_new = xw.Book()
+            sheet.copy(after=wb_new.sheets[0])
+            wb_new.sheets[0].delete()
+            wb_new.save(f'{output_path}/{base_name}_{sheet.name}.xlsx')
+            wb_new.close()
+    finally:
+        excel_app.quit()        
